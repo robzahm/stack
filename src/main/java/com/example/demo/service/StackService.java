2521c17f2abc5347d66621a2367d63875868906b
Robert Zahm <zahm_robert@cat.com>
2022-01-18 09:47:52 -0600

    Unit tests

--- src/main/java/com/example/demo/service/StackService.java
+++ src/main/java/com/example/demo/service/StackService.java
@@ -1,25 +1,53 @@
 package com.example.demo.service;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.stereotype.Component;
 
-import java.util.Stack;
+import java.util.EmptyStackException;
 
 // Use Generics?
 
 @Component
 public class StackService {
 
-    private Stack<Integer> stack = new Stack<>();
+    @AllArgsConstructor
+    @Getter
+    private class Node {
+        private Integer value;
+        private int index;
+        private Node previous;
+    }
+
+    private Node currentNode =  null;
 
     public void push(Integer val) {
-        stack.push(val);
+        if (currentNode == null) {
+            currentNode = new Node(val, 0, null);
+        }
+        else {
+            currentNode = new Node(val, currentNode.index + 1, currentNode);
+        }
     }
 
     public Integer pop() {
-        return stack.pop();
+        if (currentNode == null) {
+            throw new EmptyStackException();
+        }
+        Integer returnVal = currentNode.getValue();
+
+        currentNode = currentNode.getPrevious();
+
+        return returnVal;
     }
 
     public Integer get(int index) {
-        return stack.get(index);
+        // Throw exception if index is OOB
+
+        Node iteratorNode = currentNode;
+        while (index < iteratorNode.getIndex()) {
+            iteratorNode = iteratorNode.getPrevious();
+        }
+        return iteratorNode.getValue();
     }
 }
---
+++ src/test/java/com/example/demo/service/StackServiceTest.java
@@ -0,0 +1,35 @@
+package com.example.demo.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.EmptyStackException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class StackServiceTest {
+    @InjectMocks
+    private StackService stackService;
+
+
+    @Test
+    public void testPushGetPop() {
+        stackService.push(1);
+        stackService.push(2);
+        assertEquals(1, stackService.get(0));
+        assertEquals(2, stackService.get(1));
+
+        assertEquals(2, stackService.pop());
+        assertEquals(1, stackService.pop());
+
+        Assertions.assertThrows(
+                EmptyStackException.class, () -> {
+            stackService.pop();
+        });
+
+    }
+}
